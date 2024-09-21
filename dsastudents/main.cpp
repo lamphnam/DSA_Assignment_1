61478803888cb740e8b4e42dadfe2ecf4de9dda9
lamphnam <nampham2131@gmail.com>
2024-09-21 16:59:12 +0700

    Add initial project files

---
+++ dsastudents/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "include/list/XArrayList.h"
+#include "include/list/XArrayListDemo.h"
+
+using namespace std;
+
+int main() {
+    cout << "Testing XArrayList:" << endl;
+    xlistDemo1();  // Call the demo function directly
+    xlistDemo2();
+    xlistDemo3();
+    xlistDemo4();
+    return 0;
+}
