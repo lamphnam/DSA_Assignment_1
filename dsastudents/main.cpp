0df56d1c9cca014fa389339aba215fddf463dfb9
lamphnam <nampham2131@gmail.com>
2024-10-10 20:32:22 +0700

    add seed in shuffle and pass lms testcase

--- dsastudents/main.cpp
+++ dsastudents/main.cpp
@@ -6,42 +6,17 @@
 #include "list/XArrayList.h"
 #include "include/list/DLinkedList.h"
 #include "include/list/DLinkedListDemo.h"
+#include "include/ann/DataLoaderDemo.h"
 #include <iostream>
 
 using namespace std;
 
 // !compile: g++ -Iinclude -std=c++17 main.cpp -o output_program
 
-void dataloadertc3(){
-    xt::random::seed(10);
-    xt::xarray<double> X = xt::random::randn<double>({105, 10, 10});
-    xt::xarray<int> t = xt::ones<int>({105});
-    TensorDataset<double, int> ds(X, t);
-    DataLoader<double, int> loader(&ds, 10, false);
-    auto it = loader.begin();
-    it++;
-    cout << xt::adapt((*it).getData().shape()) << endl;
-    cout << xt::adapt((*it).getLabel().shape()) << endl;
-    cout << xt::adapt((*(++it)).getData().shape()) << endl;
-    cout << xt::adapt((*(++it)).getLabel().shape()) << endl;
-}
-
-
-void dataloadertc6(){
-    xt::random::seed(10);
-    xt::xarray<double> X = xt::random::randn<double>({100, 3, 3});
-    xt::xarray<int> t = xt::ones<int>({100});
-    TensorDataset<double, int> ds(X, t);
-    DataLoader<double, int> loader(&ds, 10, false, true);
-    for(auto batch: loader){
-        cout << batch.getData() << endl;
-        cout << batch.getLabel() << endl;
-    }
-}
-
-
 int main(int argc, char **argv) {
-    
+    case_data_wi_label_1();
+    //case_data_wo_label_1();
+    case_data_wo_label_2();
     
     return 0;
 }   
